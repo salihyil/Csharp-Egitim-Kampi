ab5e921f2bc179b1f1ca4eb38902ffc9e7b2f589
salih <salih_yil@live.com>
2024-10-27 14:50:05 +0300

    feat: Implement basic database connection and category display
    - Add SQL connection to EgitimKampiDb
    - Create a simple console menu for user interaction
    - Fetch and display categories from TblCategory
    - Implement basic error handling and connection management

---
+++ Ders-9-SQL-Veri-Tabani-ve-Tablo-Olusturma-Csharp-Kismina-Baglama/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Ders_9_SQL_Veri_Tabani_ve_Tablo_Olusturma_Csharp_Kismina_Baglama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // sql yapılarını kullanma (Ado.net)
+
+            Console.WriteLine("***** C# veri tabanlı ürün-kategori bilgi sistemi *****");
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("1-Kategoriler");
+            Console.WriteLine("2-Ürünler");
+            Console.WriteLine("3-Siparişler");
+            Console.WriteLine("4-Çıkış Yap");
+            Console.WriteLine("Lütfen girmek istediğiniz tablo numarasını giriniz: ");
+            int tableNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("--------------------------------");
+
+            SqlConnection connection = new SqlConnection("Data Source=localhost;Initial Catalog=EgitimKampiDb;Integrated Security=True");
+            connection.Open();
+            SqlCommand command = new SqlCommand("Select * from TblCategory", connection);
+            //adapter -> c# tarafındaki kodlar ile sql suunucu arasında bir köprü görevi görür.
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            Console.WriteLine(dataTable);
+            // Burdaki amaç bu sorguyu ram bellek üzerinden user'a gösterebilmek.
+
+            connection.Close();
+
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                Console.WriteLine($"{row["CategoryId"]} - {row["CategoryName"]}");
+            }
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
